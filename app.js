b365a46969de0ace448bb6fd9840957727230046
Gojira <jason@theograys.com>
2014-06-11 14:21:39 -0400

    updated to require fs for upload directory creation.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var upload = require('./routes/upload');
 var http = require('http');
+var fs = require('fs');
 var path = require('path');
 
 var app = express();
